1a74dd4ef1077ee8c341e92be49db1848ba34da4
nidhimanoj4 <nidhimanoj4@yahoo.com>
2017-11-09 21:05:51 -0800

    Commit a sh file

---
+++ baseline_model.sh
@@ -0,0 +1,3 @@
+python code/train.py --train_dir 'trains/baseline2' --load_train_dir 'trains/baseline2' \
+  --log_dir 'log/baseline' --saved_name 'baseline_bi_pass_bi_q_concatlast_answer' \
+ --sigma_threshold 0.9 --val_and_save_num_batches 200 --start_epoch 3
